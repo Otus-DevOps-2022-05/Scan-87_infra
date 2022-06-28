ee25bc3796ec8e6509dfc29df210f420d625463c
scan-87 <ascanders@gmail.com>
2022-06-28 17:37:45 +0300

    OK, my bad, coud not write to system file without sudo tee

--- deploy_full.sh
+++ deploy_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Acquire::http::Proxy "http://200.137.134.131:3128/";' >> /etc/apt/apt.conf.d/proxy.conf
-echo 'Acquire::https::Proxy "http://200.137.134.131:3128/";' >> /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::http::Proxy "http://200.137.134.131:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::https::Proxy "http://200.137.134.131:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
 
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
