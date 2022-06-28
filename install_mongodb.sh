629749b9d6c14cc5de5d743de93f6581eaae323f
scan-87 <ascanders@gmail.com>
2022-06-28 17:52:39 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Acquire::http::Proxy "http://200.137.134.131:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
-echo 'Acquire::https::Proxy "http://200.137.134.131:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::http::Proxy "http://otus:Qq123456!@89.208.142.171:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
+echo 'Acquire::https::Proxy "http://otus:Qq123456!@89.208.142.171:3128/";' | sudo tee -a /etc/apt/apt.conf.d/proxy.conf
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
